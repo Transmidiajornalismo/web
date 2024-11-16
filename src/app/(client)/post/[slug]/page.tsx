feda5a65447970f431b52ae2a9e149145b1aed03
Nathally Souza <nathallyccd@gmail.com>
2024-11-15 23:20:46 -0300

    [feat]manifesto without category

--- src/app/(client)/post/[slug]/page.tsx
+++ src/app/(client)/post/[slug]/page.tsx
@@ -78,7 +78,9 @@ export default function Post({ params }: IPostProps) {
           </div>
         </div> */}
         <div className='py-4'>
-          <span className='text-xs font-bold'>{post?.category}</span>
+          <span className='text-xs font-bold'>
+            {post?.category !== 'uncategorized' && post?.category}
+          </span>
           <h2 className='text-4xl'>{post?.title}</h2>
           <div className='flex justify-between text-gray-500'>
             <p className='text-sm'>{post?.author}</p>
