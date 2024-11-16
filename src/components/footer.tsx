a25731d2edf34caf338f81bde10658472bb8c4d4
Nathally Souza <nathallyccd@gmail.com>
2024-11-16 09:43:22 -0300

    [feat]update editorias

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import logo from '../../public/images/logo.svg';
 import { Separator } from '@/components/ui/separator';
 import { Social } from './social';
+import { EDITORIAS } from '@/constants/editorias';
 
 export const Footer = () => {
   return (
     <footer className='bg-[#CFB2F5] text-black p-4'>
       <div className='flex items-center justify-center'>
-      <Image src={logo} alt='Transmídia' className='w-[268px]'/>
+        <Image src={logo} alt='Transmídia' className='w-[268px]' />
       </div>
       <Social />
       <div className='flex justify-between mb-10'>
@@ -24,11 +25,14 @@ export const Footer = () => {
         <div className='text-right min-w-[160px]'>
           <h4 className='font-bold text-white'>Jornalismo</h4>
           <Separator className='bg-black' />
-          <ul className='text-sm font-medium leading-6'>
-            <li>Acué</li>
-            <li>Saúde</li>
-            <li>Cultura</li>
-            <li>Política</li>
+          <ul className='text-sm font-medium leading-6 capitalize'>
+            {Object.keys(EDITORIAS).map((editoria) => (
+              <li key={editoria}>
+                <a href={`/editoria/${editoria.toLowerCase()}`}>
+                  {EDITORIAS[editoria].title}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
